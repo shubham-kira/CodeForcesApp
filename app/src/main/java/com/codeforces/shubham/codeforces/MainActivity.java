b5858dc663d54726ec123268d1f7083b78307172
shubham-kira <shubham190shubham@gmail.com>
2018-10-07 15:08:32 +0530

    second commit:

--- app/src/main/java/com/codeforces/shubham/codeforces/MainActivity.java
+++ app/src/main/java/com/codeforces/shubham/codeforces/MainActivity.java
@@ -1,10 +1,20 @@
 package com.codeforces.shubham.codeforces;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     private static final String API_URL = "https://codeforces.com/api/user.rating?handle=shubham190shubham";
 
@@ -14,6 +24,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Uri uri = Uri.parse(API_URL);
-        new RatingAsync(MainActivity.this).execute(uri.buildUpon().toString());
+//        new RatingAsync(MainActivity.this).execute(uri.buildUpon().toString());
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new RatingLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        ArrayList<Contest> contestList = new ArrayList<Contest>();
+        try {
+            JSONObject basejsonResponse = new JSONObject(data);
+            JSONArray res = basejsonResponse.getJSONArray("result");
+            for(int i=0; i<res.length(); i++){
+                Contest contest = new Contest();
+                contest.setContestName(res.getJSONObject(i).getString("contestName"));
+                contest.setRank(Integer.parseInt(res.getJSONObject(i).getString("rank")));
+                contest.setOldRating(Integer.parseInt(res.getJSONObject(i).getString("oldRating")));
+                contest.setNewRating(Integer.parseInt(res.getJSONObject(i).getString("newRating")));
+                contest.setChange(contest.getNewRating()-contest.getOldRating());
+                contestList.add(contest);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        contestList = reverse(contestList);
+        RecyclerView recyclerView = findViewById(R.id.contestsAppeared);
+        ContestsAppearedAdapter adapter = new ContestsAppearedAdapter(this, contestList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+    static ArrayList<Contest> reverse(ArrayList<Contest> contestList){
+        ArrayList<Contest> newList = new ArrayList<>();
+        for(int i=contestList.size()-1; i>=0; i--)
+            newList.add(contestList.get(i));
+        return newList;
     }
 }
